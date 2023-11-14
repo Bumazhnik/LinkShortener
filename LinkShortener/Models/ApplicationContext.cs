02b80715471ea97b3e3267fafbb08bfd13a612cd
Bumazhnik <61379239+Bumazhnik@users.noreply.github.com>
2023-11-14 21:28:00 +0900

    Refactoring

--- LinkShortener/Models/ApplicationContext.cs
+++ LinkShortener/Models/ApplicationContext.cs
@@ -7,43 +7,11 @@ namespace LinkShortener.Models
 {
     public class ApplicationContext : DbContext
     {
-        public DbSet<User> Users { get; set; }
-        public DbSet<Role> Roles { get; set; }
         public DbSet<Link> Links { get; set; }
-        private IPasswordHasher<User> hasher = new PasswordHasher<User>();
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
             : base(options)
         {
             Database.EnsureCreated();
-        }
-        private User MakeAdmin(Role adminRole)
-        {
-            string adminEmail = "admin@mail.ru";
-            string adminPassword = "123456";
-            var user = new User { Email = adminEmail, Name = "admin", RoleId = adminRole.Id };
-            string hashPassword = hasher.HashPassword(user, adminPassword);
-            user.Password = hashPassword;
-            return user;
-        }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            string adminRoleName = "admin";
-            string userRoleName = "user";
-
-
-            // добавляем роли
-            Role adminRole = new Role { Id = 1, Name = adminRoleName };
-            Role userRole = new Role { Id = 2, Name = userRoleName };
-            User adminUser = MakeAdmin(adminRole);
-            adminUser.Id = 1;
-
-            /*var converter = new ValueConverter<HashSet<int>, string>(
-            v => string.Join(";", v),
-            v => new HashSet<int>(v.Split(";", StringSplitOptions.RemoveEmptyEntries).Select(val => int.Parse(val))));*/
-            modelBuilder.Entity<Role>().HasData(new Role[] { adminRole, userRole });
-            modelBuilder.Entity<User>().HasData(new User[] { adminUser });
-            //modelBuilder.Entity<Link>().HasData(new Link[] { new() { Id = 1, Address = "https://google.com" }, new() { Id = 2, Address = "https://youtube.com" } });   
-            base.OnModelCreating(modelBuilder);
         }
     }
 }
